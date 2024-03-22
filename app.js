759da7f14b1e42e92b351ca7d41b5028dd6d8dcb
SamerNassirHalat <145202162+SamerNassirHalat@users.noreply.github.com>
2024-03-22 11:15:44 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-const express = require("express")
-const app = express()
-const handlebars = require("express-handlebars").engine
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars").engine;
+const bodyParser = require("body-parser");
+const post = require("./models/post");
 
-app.engine("handlebars", handlebars({defaultLayout: "main"}))
-app.set("view engine", "handlebars")
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
-app.listen(8081,function(){
-    console.log("Servidor Ativo!")
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", function(req, res){
-    res.render("Index")
-})
+app.get("/", function (req, res) {
+    res.render("index");
+});
 
-app.get("/consultar", function(req, res){
-    res.render("consultar")
-})
+app.post("/cadastrar", function (req, res) {
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function () {
+        res.redirect("/");
+    }).catch(function (erro) {
+        res.send("Falha ao cadastrar os dados: " + erro);
+    });
+});
 
-app.get("/atualizar", function(req, res){
-    res.render("atualizar")
-})
+app.get("/consultar", function (req, res) {
+    res.render("consultar");
+});
+
+app.listen(8081, function () {
+    console.log("Servidor Ativo!");
+});
